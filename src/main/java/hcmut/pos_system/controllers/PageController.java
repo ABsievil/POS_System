8585c91c60ba5d9361c92f5553e6733f32180aa6
ABsievil <nccuong317qb@gmail.com>
2024-04-20 14:45:15 +0700

    docs(config-controller): config request to each page

--- src/main/java/hcmut/pos_system/controllers/PageController.java
+++ src/main/java/hcmut/pos_system/controllers/PageController.java
@@ -1,12 +1,14 @@
 package hcmut.pos_system.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
     
-    @RequestMapping("/")
+    @RequestMapping(value = {"/", "index"})
     public String welcome(){
         return "index";
     }
@@ -15,4 +17,47 @@ public class PageController {
     public String login(){
         return "login";
     }
+
+    @Controller
+    @RequestMapping("/update")
+    public class update{
+        @GetMapping("")
+        public String getUpdate(){
+            return "updateData";
+        }   
+
+        @PostMapping("")
+        public void postUpdate(){
+
+        }
+    }
+
+    @Controller
+    @RequestMapping("/display")
+    public class display{
+        @GetMapping("")
+        public String getDisplay(){
+            return "displayData";
+        }   
+
+        @PostMapping("")
+        public void postDisplay(){
+
+        }
+    }
+
+    @Controller
+    @RequestMapping("/callfunc")
+    public class callfunc{
+        @GetMapping("")
+        public String getCallFunc(){
+            return "callFunc";
+        }   
+
+        @PostMapping("")
+        public void postCallFunc(){
+
+        }
+    }
+
 }
