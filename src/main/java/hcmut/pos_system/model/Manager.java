1983ea86b05c063c47c0c4235a855dedcee664c1
ABsievil <nccuong317qb@gmail.com>
2024-04-22 00:56:16 +0700

    build(delete): delete BranchManage entity and move BranchManage's attribute to Manager entity

--- src/main/java/hcmut/pos_system/model/Manager.java
+++ src/main/java/hcmut/pos_system/model/Manager.java
@@ -1,7 +1,28 @@
 package hcmut.pos_system.model;
 
-public class Manager {
-    private Long id_employee;
+import java.time.LocalDate;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// @Builder
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DiscriminatorValue("MANAGER") // use only for disjoin constraints
+public class Manager extends Emloyee {
+    @OneToOne
+    @JoinColumn(name = "id_employee")
+    private Emloyee emloyee;
 
     private String degree; //bằng cấp
+
+    private LocalDate dateOfManagement;
 }
