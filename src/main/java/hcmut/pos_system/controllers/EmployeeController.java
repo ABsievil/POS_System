adeab017464aba8f207c16e5f291ea253ce43a03
ABsievil <nccuong317qb@gmail.com>
2024-05-04 00:23:55 +0700

    docs(config-PROC): config PROC 1 in lesson 1.2.3

--- src/main/java/hcmut/pos_system/controllers/EmployeeController.java
+++ src/main/java/hcmut/pos_system/controllers/EmployeeController.java
@@ -1,11 +1,15 @@
 package hcmut.pos_system.controllers;
 
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hcmut.pos_system.DTO.FindEmployeeDTO;
+import hcmut.pos_system.models.Employee;
 import hcmut.pos_system.models.ResponseObject;
 import hcmut.pos_system.services.EmployeeService;
 
@@ -19,5 +23,10 @@ public class EmployeeController {
     @GetMapping("/getAllEmployee")
     public ResponseEntity<ResponseObject> getAllEmployee() {
         return employeeService.getAllEmployee();
+    }
+
+    @GetMapping("/branch/{branchID}")
+    public ResponseEntity<ResponseObject> getEmployeesBybranchID(@PathVariable Integer branchID) {
+        return  employeeService.PROC_findEmployeesByBranchID(branchID);
     }
 }
--- src/main/java/hcmut/pos_system/services/EmployeeService.java
+++ src/main/java/hcmut/pos_system/services/EmployeeService.java
@@ -5,11 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import hcmut.pos_system.DTO.EmployeeDTO;
+import hcmut.pos_system.DTO.FindEmployeeDTO;
+import hcmut.pos_system.DTO.FindEmployeeRowMapper;
 import hcmut.pos_system.models.Employee;
 import hcmut.pos_system.models.ResponseObject;
 import hcmut.pos_system.repositories.EmployeeRepository;
@@ -18,6 +23,30 @@ import hcmut.pos_system.repositories.EmployeeRepository;
 public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public ResponseEntity<ResponseObject> PROC_findEmployeesByBranchID(Integer branchID){
+        try {
+            List<FindEmployeeDTO> employees = jdbcTemplate.query(
+            "EXEC dbo.FindEmployee @BranchID =?", new FindEmployeeRowMapper(), branchID);
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseObject("OK", "Query to find Employee By BranchID successfully", employees));
+
+        } catch (DataAccessException e) {
+            // Catch the exception raised by the RAISERROR statement
+            if (e.getMessage().contains("Không tìm thấy chi nhánh có ID") ||
+                    e.getMessage().contains("Không tìm thấy nhân viên nào thỏa mãn")) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseObject("ERROR", e.getMessage(), null));
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseObject("ERROR" + ", " + e.getMessage().toString(), "Error finding Employee By BranchID", null));
+            }
+        }
+    }
 
     // Tìm tất cả Nhân viên y tế
     public ResponseEntity<ResponseObject> getAllEmployee() {
