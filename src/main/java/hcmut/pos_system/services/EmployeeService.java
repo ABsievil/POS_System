d48be6d96a4d623dd2aa9997d292957bc8df3e11
ABsievil <nccuong317qb@gmail.com>
2024-05-06 06:29:27 +0700

    docs(query-updateInfor-page): query successfully, but Trigger not yet imported to ERROR_MESSAGE_updateEmp and update supervisorID error occur when not find employeeID = supervisorID in list

--- src/main/java/hcmut/pos_system/services/EmployeeService.java
+++ src/main/java/hcmut/pos_system/services/EmployeeService.java
@@ -83,7 +83,6 @@ public class EmployeeService {
         }
     }
 
-
     public ResponseEntity<ResponseObject> PROC_insertEmp(EmployeeDTO newEmployee){
         try {
             jdbcTemplate.update("EXEC dbo.insertemp @manv =?, @ho =?, @tenlot =?, @ten =?, @cccd =?, @sdt =?, @email =?, @luongnv =?, @nguoigiamsat =?, @machinhanh =?",  
@@ -103,7 +102,7 @@ public class EmployeeService {
 
         } catch (DataAccessException e) {
             String errorMessage = null;
-            for (String error : ERROR_MESSAGES) {
+            for (String error : ERROR_MESSAGES_insertEmp) {
                 if (e.getMessage().contains(error)) {
                     errorMessage = error;
                     break;
@@ -115,12 +114,12 @@ public class EmployeeService {
                         .body(new ResponseObject("FAILED", errorMessage, newEmployee));
             } else {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .body(new ResponseObject("ERROR" + e.getMessage().toString(), "Error inserting Employee failed", newEmployee));
+                        .body(new ResponseObject("ERROR " + e.getMessage().toString(), "Error inserting Employee failed", newEmployee));
             }
         }
     }
 
-    private static final List<String> ERROR_MESSAGES = Arrays.asList(
+    private static final List<String> ERROR_MESSAGES_insertEmp = Arrays.asList(
     "Nhân viên đã tồn tại",
     "Chi nhánh không tồn tại",
     "Nhân viên giám sát không tồn tại",
@@ -128,6 +127,69 @@ public class EmployeeService {
     "Email không hợp lệ",
     "Số điện thoại không hợp lệ",
     "Lương nhân viên phải thấp hơn lương quản lí"
+    );
+
+    public ResponseEntity<ResponseObject> PROC_updateEmp(EmployeeDTO emp){
+        try {
+            // update salary
+            jdbcTemplate.update("EXEC dbo.updatesalary @manv =?, @luongnv =?",  
+            emp.getEmployeeID(), emp.getSalary());
+            
+            //update branch
+            jdbcTemplate.update("EXEC dbo.updatebranch @manv =?, @machinhanh =?",  
+            emp.getEmployeeID(), emp.getBranchID());
+
+            //update email
+            jdbcTemplate.update("EXEC dbo.updateEmail @manv =?, @email =?",  
+            emp.getEmployeeID(), emp.getEmail());
+
+            //update cccd
+            jdbcTemplate.update("EXEC dbo.updateCCCD @manv =?, @cccd =?",  
+            emp.getEmployeeID(), emp.getCCCD());
+
+            //update PhoneNo
+            jdbcTemplate.update("EXEC dbo.updatePhoneno @manv =?, @sdt =?",  
+            emp.getEmployeeID(), emp.getPhoneNo());
+
+            //update supervisorID
+            jdbcTemplate.update("EXEC dbo.updateSupervisor @manv =?, @manguoigiamsat =?",  
+            emp.getEmployeeID(), emp.getSupervisorID());
+
+            //update Lname, Mname, Fname
+            jdbcTemplate.update("EXEC dbo.updateName @manv =?, @ho =?, @tenlot =?, @ten =?",  
+            emp.getEmployeeID(), emp.getLastName(), emp.getMiddleName(), emp.getFirstName());
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseObject("OK", "Query to update Employee successfully", null));
+
+        } catch (DataAccessException e) {
+            String errorMessage = null;
+            for (String error : ERROR_MESSAGES_updateEmp) {
+                if (e.getMessage().contains(error)) {
+                    errorMessage = error;
+                    break;
+                }
+            }
+        
+            if (errorMessage != null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new ResponseObject("FAILED", errorMessage, emp));
+            } else {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body(new ResponseObject("ERROR " + e.getMessage().toString(), "Error updating Employee failed", emp));
+            }
+        }
+    }
+
+    private static final List<String> ERROR_MESSAGES_updateEmp = Arrays.asList(
+    "Nhân viên không tồn tại",
+    "Lương nhân viên phải thấp hơn quản lí của họ",
+    "Chi nhánh không tồn tại",
+    "Email không hợp lệ",
+    "Căn cước công dân không hợp lệ",
+    "Số điện thoại không hợp lệ",
+    "Nhân viên giám sát không tồn tại",
+    "Nhân viên không thể là người giám sát chính bản thân mình"
     );
 
     public ResponseEntity<ResponseObject> PROC_deleteEmployeeById(Integer manv){
