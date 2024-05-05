e4cdde151c9cf99cf5787cfdc0f549237f1d0812
ABsievil <nccuong317qb@gmail.com>
2024-05-05 12:42:48 +0700

    docs(add-PROC): add FindEmployeeByEmployeeID in db

--- src/main/resources/db/Employee.sql
+++ src/main/resources/db/Employee.sql
@@ -7,3 +7,16 @@ END
 EXEC dbo.EmployeeInfor 
 
 DROP PROCEDURE EmployeeInfor
+
+CREATE PROCEDURE FindEmployeeByEmployeeID 
+ @EmployeeID INT
+AS
+BEGIN
+	SELECT *
+	FROM EMPLOYEE
+	WHERE EmployeeID = @EmployeeID
+END
+
+EXEC dbo.FindEmployeeByEmployeeID 
+@EmployeeID = 1
+DROP PROCEDURE FindEmployeeByEmployeeID
