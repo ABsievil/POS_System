df62300fed6c9d00d027810d9088a9af751f2b08
LemonHV <matruongvu1@gmail.com>
2024-05-06 21:19:45 +0700

    docs(entity.sql): add procedure callNormalEmployee

--- src/main/resources/db/entity.sql
+++ src/main/resources/db/entity.sql
@@ -8,3 +8,19 @@ end;
 exec callsupplier
 -------------------------
 drop proc callSupplier
+
+
+CREATE PROCEDURE callNormalEmployee
+AS
+BEGIN
+	SELECT EmployeeID, CONCAT(LastName,' ', MiddleName,' ',FirstName) AS FullName, CCCD, PhoneNo, Email, Salary, SupervisorID, BranchID
+	FROM Employee AS E
+	WHERE 
+			E.EmployeeID NOT IN (SELECT EmployeeID FROM Cashier) AND
+			E.EmployeeID NOT IN (SELECT EmployeeID FROM Manager) AND
+			NOT EXISTS (SELECT SupervisorID FROM Employee WHERE SupervisorID = E.EmployeeID)
+END
+----------------------
+EXEC callNormalEmployee
+-------------------------
+DROP callNormalEmployee
