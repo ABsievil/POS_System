de4bb09ddc61acd1d310afc92f7ccf7608739926
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-05-06 22:44:14 +0700

    docs(mod-chk-file): add check constraint

--- src/main/resources/db/Check.sql
+++ src/main/resources/db/Check.sql
@@ -2,7 +2,6 @@
 ALTER TABLE Employee
 ADD CONSTRAINT CHK_SalaryRange CHECK (Salary >= 5);
 
-
 -- Constraint on available shift's time
 ALTER TABLE [Shift]
 ADD CONSTRAINT CHK_ShiftTimeRange CHECK (ShiftTime IN (1, 2, 3));
@@ -14,3 +13,15 @@ ADD CONSTRAINT CHK_DiscountRange CHECK (Discount >= 5 AND Discount <= 90);
 -- Constraint on the number of each product type in a batch
 ALTER TABLE ImportBatch
 ADD CONSTRAINT CHK_BatchQuantityRange CHECK (ProductQuantity > 0 AND ProductQuantity <= 3000);
+
+-- Constraint on typing speed of a cashier
+ALTER TABLE Cashier
+CONSTRAINT CHK_CashierTypingSpeed CHECK (TypingSpeed > 0 AND TypingSpeed <= 10);
+
+-- Constraint on order date of a bill
+ALTER TABLE Bill
+CONSTRAINT CHK_Bill_OrderDate CHECK (OrderDate <= GETDATE());
+
+-- Constraint on warranty period of equipment
+ALTER Equipments
+CONSTRAINT CHK_Equipments_WarrantyPeriod CHECK (WarrantyPeriod > 0);
